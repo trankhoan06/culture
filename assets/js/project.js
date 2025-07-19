6f5558dc2291a826225a98fa5fc63e1d6f6ac428
Khanh0601 <kelvin@bear.plus>
2025-07-19 12:51:39 +0700

    update project

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,14 +1,18 @@
 const parseRem = (input) => {
   return (input / 10) * parseFloat($("html").css("font-size"));
 };
-
+const viewport = {
+  w: window.innerWidth,
+  h: window.innerHeight,
+};
 function projectHero() {
   let lastIndex=0;
+  let scrollbar = $(window).width() > 767 ? ".project_hero_control_progress.hidden-mob" : '.project_hero_swiper .project_hero_control_progress' ;
   let swiperHero = new Swiper(".project_hero_swiper", {
     slidesPerView: 1,
     spaceBetween: parseRem(20),
     scrollbar: {
-      el: ".project_hero_control_progress",
+      el: scrollbar,
     },
     navigation: {
       nextEl: ".project_hero_control_next",
@@ -26,7 +30,16 @@ function projectHero() {
     }
   });
   let title =  new SplitType('.project_hero_content_title',{types: 'lines, words', lineClass: 'kl-line'})
-  gsap.set('.project_hero_content_title:not(:first-child) .word', {yPercent: 100})
+  let sub =  new SplitType('.project_hero_content_sub',{types: 'lines, words', lineClass: 'kl-line'})
+  let label =  new SplitType('.project_hero_content_label',{types: 'lines, words', lineClass: 'kl-line'})
+  gsap.set('.project_hero_content_title .word', {yPercent: 101})
+  gsap.set('.project_hero_content_sub .word', {yPercent: 100})
+  gsap.set('.project_hero_content_label .word', {yPercent: 100})
+  gsap.set('.project_hero_content_btn ', {y: 40, autoAlpha: 0})
+  gsap.set('.project_hero_control ', {y: 40, autoAlpha: 0})
+  gsap.set('.project_hero_content_btn ', {y: 40, autoAlpha: 0})
+  gsap.set('.project_hero_swiper_slide ', {x: -40, autoAlpha: 0})
+  gsap.set('.project_hero_bg_inner.active ', {scale: 1.3})
   let height = $('.project_hero_content_title').eq(0).outerHeight(true);
   $('.project_hero_content_title_wrap').height(height);
   function activeTitle (index) {
@@ -46,5 +59,54 @@ function projectHero() {
     $('.project_hero_content_title').removeClass('active')
     $('.project_hero_content_title').eq(index).addClass('active')
   }
+  let tl = gsap.timeline({
+    onStart: () => {
+      $('.df_hide_onload').removeClass('df_hide_onload');
+    }
+  });
+  tl  
+    .to('.project_hero_bg_inner.active', {scale:1, duration: 2})
+    .to('.project_hero_content_label .word', {yPercent:0, duration: .6, stagger: .02},'<=0')
+    .to('.project_hero_content_title:first-child .word', {yPercent:0, duration: .6, stagger: .025},'<=.4')
+    .to('.project_hero_content_sub .word', {yPercent:0, duration: .4, stagger: .015},'<=.2')
+    .to('.project_hero_swiper_slide', {x:0, autoAlpha:1, duration: .6, stagger: .1},'<=0')
+    .to('.project_hero_content_btn', {y:0, autoAlpha:1, duration: .6, stagger: .015},'<=0')
+    .to('.project_hero_control', {y:0, autoAlpha:1, duration: .6, stagger: .015},'<=.2')
 }
 projectHero();
+function projectRealstate() {
+  let lastIndex =0;
+  let title =  new SplitType('.project__realestate__smalltitle',{types: 'lines, words', lineClass: 'kl-line'})
+  gsap.set('.project__realestate__smalltitle:not(:first-child) .word', {yPercent: 100});
+  function activeTitle (index) {
+    if(index > lastIndex) {
+      gsap.to('.project__realestate__smalltitle.active .word', {yPercent: -100, stagger: .025})
+      gsap.to($('.project__realestate__smalltitle').eq(index).find('.word'), {yPercent: 0, stagger: .025})
+    }
+    else{
+      gsap.to('.project__realestate__smalltitle.active .word', {yPercent: 100, stagger: .025})
+      gsap.to($('.project__realestate__smalltitle').eq(index).find('.word'), {yPercent: 0, stagger: .025})
+    }
+    $('.project__realestate__smalltitle').removeClass('active')
+    $('.project__realestate__smalltitle').eq(index).addClass('active')
+  }
+  $('.project__realestate__menu__item').on('click', function(){
+    let index = $(this).index();
+    lastIndex = index;
+    $('.project__realestate__menu__item').removeClass('active')
+    $(this).addClass('active');
+    activeTitle(index);
+    $('.project__realestate__detail__inner').removeClass('active');
+    $('.project__realestate__detail__inner').eq(index).addClass('active');
+    $('.project__realestate__detail__inner.active').show(500);
+  })
+  if ($(window).width() < 991) {
+    $('.project__realestate__menu').addClass('swiper')
+    $('.project__realestate__menu__inner').addClass('swiper-wrapper')
+    $('.project__realestate__menu__item').addClass('swiper-slide');
+    let swiperTab = new Swiper(".project__realestate__menu", {
+      slidesPerView: 'auto'
+    });
+  }
+}
+projectRealstate();
