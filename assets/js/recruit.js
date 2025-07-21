250891aef874245471d99406a7f6d8a3f821ee0d
Khanh0601 <kelvin@bear.plus>
2025-07-21 08:12:50 +0700

    update

--- assets/js/recruit.js
+++ assets/js/recruit.js
@@ -1,5 +1,18 @@
 $('.recruit__opportunity__table__row__inner').click(function () {
-   $(this).closest('.recruit__opportunity__table__row')
-           .find('.recruit__opportunity__table__row__detail')
-           .slideToggle();
-  });
+        
+        if($(this).hasClass('active')){
+                $(this).removeClass('active');
+                $(this).closest('.recruit__opportunity__table__row')
+                .find('.recruit__opportunity__table__row__detail')
+                .slideUp();
+        }
+        else {
+                $('.recruit__opportunity__table__row__inner').removeClass('active')
+                $(this).addClass('active')
+                $('.recruit__opportunity__table__row__detail')
+                        .slideUp();
+                $(this).closest('.recruit__opportunity__table__row')
+                        .find('.recruit__opportunity__table__row__detail')
+                        .slideDown();
+        }
+});
--- assets/js/relationship.js
+++ assets/js/relationship.js
@@ -1,16 +1,41 @@
 $(document).ready(function() {
-  $('.relationship__content__history__year__item').click(function() {
-    var selectedYear = $(this).data('year'); // đúng cú pháp
-
-    // Bỏ active trước đó
-    $('.relationship__content__history__year__item').removeClass('active');
-    $(this).addClass('active');
-
-    // Ẩn tất cả nội dung và hiện nội dung năm được chọn
-    $('.relationship__content__item').removeClass('active');
-    $('.relationship__content__item[data-year="' + selectedYear + '"]').addClass('active');
-  });
-
-  // Hiển thị mặc định năm 2025 (nếu có)
+  
   $('.relationship__content__history__year__item[data-year="2025"]').click();
+  if($(document).width() < 768) {
+    $('.relationship__content__history__time').addClass('swiper')
+    $('.relationship__content__history__year').addClass('swiper-wrapper')
+    $('.relationship__content__history__year__item').addClass('swiper-slide')
+    let totalSlides = $('.relationship__content__history__time .swiper-slide').length;
+    let swiperYear = new Swiper('.relationship__content__history__time', {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      initialSlide: totalSlides - 1,
+      on: {
+        slideChange: function () {
+          let currentIndex = this.realIndex;
+          let selectedYear = $('.relationship__content__history__year__item').eq(currentIndex).data('year');
+    
+          $('.relationship__content__item').removeClass('active');
+          $('.relationship__content__item[data-year="' + selectedYear + '"]').addClass('active');
+    
+        }
+      }
+    })
+    $('.relationship__content__history__year__item').click(function() {
+      let index = $(this).index();
+      swiperYear.slideTo(index, 500);
+      var selectedYear = $(this).data('year'); 
+      $('.relationship__content__item').removeClass('active');
+      $('.relationship__content__item[data-year="' + selectedYear + '"]').addClass('active');
+    });
+  }
+  else {
+    $('.relationship__content__history__year__item').click(function() {
+      var selectedYear = $(this).data('year'); // đúng cú pháp
+      $('.relationship__content__history__year__item').removeClass('active');
+      $(this).addClass('active');
+      $('.relationship__content__item').removeClass('active');
+      $('.relationship__content__item[data-year="' + selectedYear + '"]').addClass('active');
+    });
+  }
 });
