d73185dcaba356a545ea3f30595b830ddb0c3826
trankhoan06 <155511953+trankhoan06@users.noreply.github.com>
2025-07-23 23:36:30 +0700

    fix

--- assets/js/recruit.js
+++ assets/js/recruit.js
@@ -1,3 +1,4 @@
+$(document).ready(function () {
 $('.recruit__opportunity__table__row__inner').click(function () {
         
         if($(this).hasClass('active')){
@@ -16,3 +17,21 @@ $('.recruit__opportunity__table__row__inner').click(function () {
                         .slideDown();
         }
 });
+     $(".recruit__opportunity__apply").on("click", function (e) {
+    e.stopPropagation();
+    $(".recruit__opportunity__form").addClass("active");
+  });
+
+  // Ngăn không đóng khi click trong form
+  $(".recruit__opportunity__form__inner").on("click", function (e) {
+    e.stopPropagation();
+  });
+
+  // Click bên ngoài form => ẩn
+  $(document).on("click", function (e) {
+    // Nếu click KHÔNG nằm trong .recruit__opportunity__form__inner
+    if (!e.target.closest(".recruit__opportunity__form__inner")) {
+      $(".recruit__opportunity__form").removeClass("active");
+    }
+  });
+  });
